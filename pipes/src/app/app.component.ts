05dc808c2888d7cb1dfa55dec7601aacca02ad8f
bdushimi <lespharaons@hotmail.com>
2022-05-20 00:55:12 +0200

    Adds custom pipe

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  name = 'bernard dushimimana';
+  todaysDate = new Date();
+  cost = 2000;
+  pizza = {
+    toppings: ['pepperoni', 'mushroom'],
+    size: 'Large',
+  };
 }
--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DoublePipe } from './double.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoublePipe
   ],
   imports: [
     BrowserModule,
